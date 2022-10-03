44c126556fd955b6fdc83c6785c5166507264393
Moreed Subat <moreeds4@gmail.com>
2022-10-03 15:22:43 -0700

    Reporting commit

---
+++ src/test/java/TestOne/Reporting.java
@@ -0,0 +1,13 @@
+package TestOne;
+
+import org.testng.annotations.Test;
+
+public class Reporting {
+
+    @Test
+    void reportingProcess()
+    {
+        System.out.println("reporting with TestNG");
+    }
+
+}
